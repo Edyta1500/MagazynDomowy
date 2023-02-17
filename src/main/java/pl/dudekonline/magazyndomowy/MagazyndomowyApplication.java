316dcd081a72c2b0ea1f63132d8da9bbb323ed83
Edyta Dudek <edyta.dudek@10g.pl>
2023-02-17 16:16:57 +0100

    Initial commit

---
+++ src/main/java/pl/dudekonline/magazyndomowy/MagazyndomowyApplication.java
@@ -0,0 +1,13 @@
+package pl.dudekonline.magazyndomowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazyndomowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazyndomowyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dudekonline/magazyndomowy/MagazyndomowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dudekonline.magazyndomowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazyndomowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
